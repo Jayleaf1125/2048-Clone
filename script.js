693abef4498eae67c4aa595a66f406e41531ba6a
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-03-08 23:02:45 -0500

    First Half Complete

---
+++ script.js
@@ -0,0 +1,8 @@
+import Grid from "./grid.js";
+
+const gameBoard = document.querySelector(`#game-board`);
+
+const grid = new Grid(gameBoard);
+console.log(grid.randomEmptyCell())
+// grid.randomEmptyCell().tile = new Tile(gameBoard);
+// grid.randomEmptyCell().tile = new Tile(gameBoard);
